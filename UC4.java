3836191a37ad1abcfe9b246bef7a5e5615c6002e
agrimmodgil30 <agrimmodgil30@gmail.com>
2026-02-23 01:49:22 +0530

    Coded to render OOPS as banner using String Array and Loop

--- UC4.java
+++ UC4.java
@@ -2,10 +2,10 @@ public class UC4 {
 
     public static void main(String[] args) {
 
-        // String array to store all 7 lines of the OOPS banner
+        
         String[] bannerLines = new String[7];
 
-        // Populate each line using String.join()
+       
         bannerLines[0] = String.join("", " *** ", "  ", " *** ", "  ", "***  ", "  ", " ****");
         bannerLines[1] = String.join("", "*   *", "  ", "*   *", "  ", "*   *", "  ", "*    ");
         bannerLines[2] = String.join("", "*   *", "  ", "*   *", "  ", "*   *", "  ", "*    ");
@@ -14,7 +14,7 @@ public class UC4 {
         bannerLines[5] = String.join("", "*   *", "  ", "*   *", "  ", "*    ", "  ", "    *");
         bannerLines[6] = String.join("", " *** ", "  ", " *** ", "  ", "*    ", "  ", "***  ");
 
-        // Use for-each loop to print each line of the banner
+        
         for (String line : bannerLines) {
             System.out.println(line);
         }
